ba69b865b8cf129689dc34a92c8957d5de02ab84
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2025-10-06 15:47:03 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -8,3 +8,7 @@ T = float(input("Enter the time (in years): "))
 # Calculate Simple Interest
 SI = (P * R * T) / 100
 print("Simple Interest: ₹", round(SI, 2))
+# Calculate Compound Interest
+A = P * (1 + R / 100) ** T
+CI = A - P
+print("Compound Interest: ₹", round(CI, 2))
